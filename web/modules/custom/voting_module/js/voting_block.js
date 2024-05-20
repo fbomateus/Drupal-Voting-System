3e96f1649852fed5a05f40a0253f6518249a8f69
Fabio Mateus <fabiomateus.jobs@gmail.com>
2024-05-20 03:59:56 -0300

    feat(js): send selected_option with vote submission and reload page on success

--- web/modules/custom/voting_module/js/voting_block.js
+++ web/modules/custom/voting_module/js/voting_block.js
@@ -4,63 +4,56 @@
  */
 
 (function ($, Drupal) {
+  if (!$.fn.once) {
+    $.fn.once = function (id) {
+      return this.filter(function () {
+        if (!this.once) {
+          this.once = {};
+        }
+        if (!this.once[id]) {
+          this.once[id] = true;
+          return true;
+        }
+        return false;
+      });
+    };
+  }
+
   Drupal.behaviors.votingBlock = {
     attach: function (context, settings) {
-      $('#question-select', context).once('votingBlock').change(function () {
-        var questionId = $(this).val();
-        $.ajax({
-          url: Drupal.url('api/voting_module/get_answers/' + questionId),
-          type: 'GET',
-          dataType: 'json',
-          success: function (data) {
-            var answersContainer = $('#answers-container');
-            answersContainer.empty();
-            if (data.length) {
-              data.forEach(function (answer) {
-                answersContainer.append(
-                  '<div class="form-check">' +
-                  '<input class="form-check-input" type="radio" name="answer_id" id="answer-' + answer.id + '" value="' + answer.id + '">' +
-                  '<label class="form-check-label" for="answer-' + answer.id + '">' + answer.label + '</label>' +
-                  '</div>'
-                );
-              });
-            } else {
-              answersContainer.append('<p>' + Drupal.t('No answers available for this question.') + '</p>');
-            }
-          },
-          error: function () {
-            var answersContainer = $('#answers-container');
-            answersContainer.empty();
-            answersContainer.append('<p>' + Drupal.t('An error occurred while loading the answers. Please try again.') + '</p>');
-          }
-        });
-      }).trigger('change'); // Trigger change event on page load to load answers for the first question.
+      $('.toggle-answer-options', context).once('votingBlock').click(function () {
+        var questionId = $(this).data('question-id');
+        $('#answer-options-' + questionId).toggle();
+      });
 
-      // Handle form submission via AJAX
-      $('#voting-block-form', context).once('votingBlock').submit(function (event) {
+      $('.vote-form', context).once('votingBlock').submit(function (event) {
         event.preventDefault();
-
         var form = $(this);
-        var formData = form.serialize();
+        var questionId = form.data('question-id');
+        var answerId = form.find('input[name="answer_option"]:checked').val();
+        var selectedOption = form.find('input[name="answer_option"]:checked').parent().text().trim();
+
+        if (!answerId) {
+          alert(Drupal.t('Please select an answer option.'));
+          return;
+        }
 
         $.ajax({
           url: Drupal.url('api/voting_module/vote'),
           type: 'POST',
-          data: formData,
+          data: JSON.stringify({
+            question_id: questionId,
+            answer_id: answerId,
+            selected_option: selectedOption
+          }),
+          contentType: 'application/json; charset=utf-8',
           dataType: 'json',
           success: function (response) {
-            form[0].reset();
-            $('#answers-container').empty();
-            if (response.message) {
-              form.prepend('<div class="status-message">' + response.message + '</div>');
-            }
+            alert(Drupal.t('Your vote has been submitted.'));
+            location.reload();
           },
-          error: function (xhr) {
-            var errorMessage = Drupal.t('An error occurred while submitting your vote. Please try again.');
-            if (xhr.responseJSON && xhr.responseJSON.message) {
-              errorMessage = xhr.responseJSON.message;
-            }
-            form.prepend('<div class="error-message">' + errorMessage + '</div>');
+          error: function () {
+            alert(Drupal.t('There was an error submitting your vote. Please try again.'));
           }
         });
       });
